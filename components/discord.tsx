097e631bd6f0cfb3b44bfad51e73f566b3a793b8
Anish Pallati <anishp@duck.com>
2023-07-20 13:30:54 -0400

    style: Adjust discord embed sizing on mobile

--- components/discord.tsx
+++ components/discord.tsx
@@ -18,7 +18,7 @@ export const Discord = () => {
 	return (
 		<iframe
 			src={src}
-			className="h-[250px] w-full sm:h-[500px] outline-none border-[#5865f2] border-[2px] rounded-lg"
+			className="w-full h-[500px] outline-none border-[#5865f2] border-[2px] rounded-lg"
 			sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
 		></iframe>
 	);
