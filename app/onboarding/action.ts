e3f1a1d9049c31ce548b7776609693313dd603dd
Anish Pallati <anishp@duck.com>
2023-07-26 22:39:25 -0400

    feat: Create server action for onboarding

---
+++ app/onboarding/action.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { insertUser } from "@/db/client";
+import { type FormInput } from "@/app/onboarding/form";
+import { clerkClient, type useAuth } from "@clerk/nextjs";
+
+type UserId = {
+	userId: ReturnType<typeof useAuth>["userId"];
+};
+
+export async function saveResponse(data: FormInput & UserId) {
+	const { userId, ...responses } = data;
+
+	if (!userId) return;
+
+	await insertUser({
+		id: userId,
+		responses,
+	});
+
+	await clerkClient.users.updateUser(userId, {
+		privateMetadata: {
+			onboardingCompleted: true,
+		},
+	});
+}
