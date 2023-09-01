7cb0c63f0514bfa36b94617d59b074e2e0235f87
AC_is_Cool <choudharyaditya974@gmail.com>
2023-09-01 10:16:38 -0400

    finished the workshoplist

--- app/workshops/page.tsx
+++ app/workshops/page.tsx
@@ -32,25 +32,23 @@ export default function Workshop() {
 
 
 				<Heading>Want to learn more about Coding!</Heading>
+
+			</Content>
+			<Content
+				as="div"
+				className="py-16"
+				outerClassName="relative"
+				border="bottom"
+			>
 				<h2 className="my-4 text-2xl font-thin">
 					hack.place() is happy to present a few workshops to
 					{" "} <Hover>increase your knowledge</Hover> {" "} of this world
 
 				</h2>
 
-				<div className="absolute bottom-0 right-0 translate-y-24 text-muted -z-10">
-					<Anchor
-						className="w-72 h-72 rotate-[15deg]"
-						strokeWidth={0.75}
-					/>
-				</div>
+
 			</Content>
 			<Content as="section" className="py-16" border="bottom">
-
-				<h1 className="p-4 mx-auto text-6xl font-bold text-center w-fit rounded-2xl bg-brand text-sky-900">
-					Workshops
-				</h1>
-
 				<WorkshopsList></WorkshopsList>
 			</Content>
 
--- app/workshops/workshop.tsx
+++ app/workshops/workshop.tsx
@@ -1,198 +1,138 @@
 "use client";
 
 import { useRef, type PropsWithChildren, type MouseEventHandler } from "react";
-
+import NextLink from "next/link";
 import { Link } from "@/components/link";
 import { Hover } from "@/components/utils";
 import { CardItem } from "@/components/card";
+import { Github } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 interface CostItemProps {
-	name: string;
-	strikethroughCost?: string;
-	actualCost: string;
-	label?: string;
+    name: string;
+    gitLink: string;
+    label?: string;
 }
 
 
 const CostItem = ({
-	name,
-	strikethroughCost,
-	actualCost,
-	label,
-	children,
+    name,
+    label,
+    gitLink,
+    children,
 }: PropsWithChildren<CostItemProps>) => {
-	return (
-		<div className="flex flex-col justify-between h-full">
-				<h2 className="text-xl font-semibold">{name}</h2>
-				<h1 className="text-3xl font-bold">
-					<Hover>
-						{strikethroughCost && (
-							<>
-								<span className="line-through">
-									{strikethroughCost}
-								</span>{" "}
-							</>
-						)}
-						{actualCost}
-					</Hover>{" "}
-					<span className="text-2xl text-muted-foreground">
-						{label}
-					</span>
-				</h1>
-
-			<h3 className="ml-auto text-sm text-muted-foreground">
-				{children}
-			</h3>
-		</div>
-	);
+    return (
+        <div className="flex flex-col justify-between h-full">
+            <div className="flex flex-row justify-between h-full">
+                <h2 className="text-xl font-semibold">{name}</h2>
+                <NextLink key={"github"} href={gitLink} target="_blank">
+					<Button
+						size="icon"
+						variant="ghost"
+						className={cn("transition-colors", "hover:text-brand")}
+					>
+						<Github></Github>
+					</Button>
+				</NextLink>
+            </div>
+            <div>
+                <h2 className="text-s">{label}</h2>
+            </div>
+        </div>
+    );
 };
 
 export const WorkshopsList = () => {
-	const ref1 = useRef<HTMLDivElement>(null);
-	const ref2 = useRef<HTMLDivElement>(null);
-	const ref3 = useRef<HTMLDivElement>(null);
-	const ref4 = useRef<HTMLDivElement>(null);
-	const ref5 = useRef<HTMLDivElement>(null);
-	const ref6 = useRef<HTMLDivElement>(null);
-	const ref7 = useRef<HTMLDivElement>(null);
-	const ref8 = useRef<HTMLDivElement>(null);
-	const ref9 = useRef<HTMLDivElement>(null);
+    const ref1 = useRef<HTMLDivElement>(null);
+    const ref2 = useRef<HTMLDivElement>(null);
+    const ref3 = useRef<HTMLDivElement>(null);
+    const ref4 = useRef<HTMLDivElement>(null);
+    const ref5 = useRef<HTMLDivElement>(null);
+    const ref6 = useRef<HTMLDivElement>(null);
+    const ref7 = useRef<HTMLDivElement>(null);
+    const ref8 = useRef<HTMLDivElement>(null);
+    const ref9 = useRef<HTMLDivElement>(null);
+    const workshops = [
+        {
+            title: "Intro to Web Development",
+            friendlyName: "Weather App",
+            descr: "This code accompanies the Intro to Web Development workshop for hack.place(). For a step-by-step guide on how to create this app, please reference the accompanying presentation.",
+            gitLink: "https://github.com/hackplace-org/weather-app",
+            ref: ref1
+        },
+        {
+            title: "Intro to HTML/CSS/JS",
+            friendlyName: "Calculator App",
+            descr: "This code accompanies the Intro to HTML/CSS/JS workshop for hack.place(). For a step-by-step guide on how to create this app, please reference the accompanying presentation.",
+            gitLink: "https://github.com/hackplace-org/calculator-app",
+            ref: ref2
+        },
+        {
+            title: "Intro to Chrome Extensions",
+            friendlyName: "Color Palette Generator",
+            descr: "This code accompanies the Intro to Chrome Extensions workshop for hack.place(). For a step-by-step guide on how to create this app, please reference the accompanying presentation.",
+            gitLink: "https://github.com/hackplace-org/color-palette-generator",
+            ref: ref3
+        },
+        {
+            title: "Object-Oriented Python",
+            friendlyName: "Discord Bot",
+            descr: "This code accompanies the Object-Oriented Python workshop for hack.place(). For a step-by-step guide on how to create this app, please reference the accompanying presentation.",
+            gitLink: "https://github.com/hackplace-org/discord-bot",
+            ref: ref4
+        },
+        {
+            title: "Intro to React Native",
+            friendlyName: "Image Filter App",
+            descr: "This code accompanies the Intro to React Native workshop for hack.place(). For a step-by-step guide on how to create this project, please reference the accompanying presentation.",
+            gitLink: "https://github.com/hackplace-org/image-filter-app",
+            ref: ref5
+        },
+        {
+            title: "Minecraft Modding",
+            friendlyName: "Magic Mod",
+            descr: "This code accompanies the Minecraft Modding workshop for hack.place(). For a step-by-step guide on how to create this project, please reference the accompanying presentation.",
+            gitLink: "https://github.com/hackplace-org/magic-mod",
+            ref: ref6
+        },
+    ]
 
-	const onMouseMove: MouseEventHandler<HTMLDivElement> = (e) => {
-		[ref1, ref2, ref3, ref4, ref5, ref6, ref7, ref8, ref9].forEach(
-			(ref) => {
-				const card = ref.current;
-				if (!card) return;
+    const onMouseMove: MouseEventHandler<HTMLDivElement> = (e) => {
+        [ref1, ref2, ref3, ref4, ref5, ref6, ref7, ref8, ref9].forEach(
+            (ref) => {
+                const card = ref.current;
+                if (!card) return;
 
-				const rect = card.getBoundingClientRect(),
-					x = e.clientX - rect.left,
-					y = e.clientY - rect.top;
+                const rect = card.getBoundingClientRect(),
+                    x = e.clientX - rect.left,
+                    y = e.clientY - rect.top;
 
-				card.style.setProperty("--mouse-x", `${x}px`);
-				card.style.setProperty("--mouse-y", `${y}px`);
-			}
-		);
-	};
+                card.style.setProperty("--mouse-x", `${x}px`);
+                card.style.setProperty("--mouse-y", `${y}px`);
+            }
+        );
+    };
 
-	return (
-		<div
-			onMouseMove={onMouseMove}
-			className="grid grid-cols-1 gap-2 mt-8 group/cards grid-rows-9 sm:grid-cols-2 sm:grid-rows-5 md:grid-rows-3 md:grid-cols-3"
-		>
-            
-			<CardItem ref={ref1}>
-				<CostItem
-					name="Google Workspace"
-					strikethroughCost="$12"
-					actualCost="$0"
-					label="per user / month"
-				>
-					(thanks to{" "}
-					<Link
-						href="https://hackclub.com/"
-						text="Hack Club"
-						external
-					/>
-					)
-				</CostItem>
-			</CardItem>
+    return (
+        <div
+            onMouseMove={onMouseMove}
+            className="grid grid-cols-2 gap-2 "
+        >
+            {workshops.map((workshop, i) => (
+                <CardItem ref={workshop.ref}>
+                    <CostItem
+                        gitLink={workshop.gitLink}
+                        name={workshop.title}
+                        label={workshop.descr}
+                    >
+                    </CostItem>
+                </CardItem>
 
-			<CardItem ref={ref2}>
-				<CostItem
-					name="Google Domains"
-					actualCost="$12"
-					label="per year"
-				>
-					(not including email)
-				</CostItem>
-			</CardItem>
+            ))
 
-			<CardItem ref={ref3}>
-				<CostItem
-					name="Sendy"
-					strikethroughCost="$69 + $1"
-					actualCost="$0"
-					label="per 10,000 emails"
-				>
-					(thanks to{" "}
-					<Link
-						href="https://hackclub.com/"
-						text="Hack Club"
-						external
-					/>
-					)
-				</CostItem>
-			</CardItem>
+            }
 
-			<CardItem ref={ref4}>
-				<CostItem
-					name="Vercel"
-					strikethroughCost="$20"
-					actualCost="$0"
-					label="per user / month"
-				>
-					(limited Hobby tier)
-				</CostItem>
-			</CardItem>
-
-			<CardItem ref={ref5}>
-				<CostItem
-					name="Clerk"
-					strikethroughCost="$35"
-					actualCost="$0"
-					label="per month"
-				>
-					(limited Free tier)
-				</CostItem>
-			</CardItem>
-
-			<CardItem ref={ref6}>
-				<CostItem
-					name="Turso"
-					strikethroughCost="$29"
-					actualCost="$0"
-					label="per month"
-				>
-					(limited Starter tier)
-				</CostItem>
-			</CardItem>
-
-			<CardItem ref={ref7}>
-				<CostItem
-					name="Zoom Pro"
-					strikethroughCost="$149"
-					actualCost="$0"
-					label="per year"
-				>
-					(thanks to{" "}
-					<Link
-						href="https://hackclub.com/"
-						text="Hack Club"
-						external
-					/>
-					)
-				</CostItem>
-			</CardItem>
-
-			<CardItem ref={ref8}>
-				<CostItem
-					name="Instagram"
-					actualCost="$99"
-					label="per 30-day advertisement"
-				>
-					(estimated 960-2760 clicks)
-				</CostItem>
-			</CardItem>
-
-			<CardItem ref={ref9}>
-				<CostItem
-					name="Hackathon Prize Valuation"
-					actualCost="$2,500"
-					label="per hackathon"
-				>
-					(sponsor&apos;s prizes + bought ourselves)
-				</CostItem>
-			</CardItem>
-		</div>
-	);
+        </div>
+    );
 };
