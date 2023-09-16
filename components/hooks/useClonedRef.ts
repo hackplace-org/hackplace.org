e3a8f6ac26ba2427a260bd73f0a19618a8bad6d3
Anish Pallati <anishp@duck.com>
2023-09-16 11:24:12 -0400

    fix: Remove unnecessary any

--- components/hooks/useClonedRef.ts
+++ components/hooks/useClonedRef.ts
@@ -1,10 +1,7 @@
-import { useRef, useEffect, type ForwardedRef } from "react";
+import { type ForwardedRef, useEffect, useRef } from "react";
 
 // https://github.com/facebook/react/issues/24722
-export const useClonedRef = <T>(
-	ref: ForwardedRef<T>,
-	initialValue: any = null
-) => {
+export const useClonedRef = <T>(ref: ForwardedRef<T>, initialValue = null) => {
 	const targetRef = useRef<T>(initialValue);
 
 	useEffect(() => {
