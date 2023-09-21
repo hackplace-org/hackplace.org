f025bac61c8334727d7c673d29e94d504e2ef765
Anish Pallati <anishp@duck.com>
2023-09-20 20:43:22 -0400

    feat: Temporarily disable onboarding action

--- app/onboarding/action.ts
+++ app/onboarding/action.ts
@@ -1,31 +1,31 @@
 "use server";
 
-import type { FormInput, OnboardingFormProps } from "@/app/onboarding/form";
+// import type { FormInput, OnboardingFormProps } from "@/app/onboarding/form";
 import { insertUser } from "@/db/client";
 import { clerkClient, type useAuth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-type UserId = {
-	userId: ReturnType<typeof useAuth>["userId"];
-	redirectTo: OnboardingFormProps["redirectTo"];
-};
+// type UserId = {
+// 	userId: ReturnType<typeof useAuth>["userId"];
+// 	redirectTo: OnboardingFormProps["redirectTo"];
+// };
 
-export async function saveResponse(data: FormInput & UserId) {
-	const { userId, redirectTo, ...responses } = data;
+// export async function saveResponse(data: FormInput & UserId) {
+// 	const { userId, redirectTo, ...responses } = data;
 
-	if (!userId) return;
+// 	if (!userId) return;
 
-	await insertUser({
-		id: userId,
-		responses,
-	});
+// 	await insertUser({
+// 		id: userId,
+// 		responses,
+// 	});
 
-	await clerkClient.users.updateUser(userId, {
-		privateMetadata: {
-			onboardingCompleted: true,
-			nonce: Date.now(),
-		},
-	});
+// 	await clerkClient.users.updateUser(userId, {
+// 		privateMetadata: {
+// 			onboardingCompleted: true,
+// 			nonce: Date.now(),
+// 		},
+// 	});
 
-	redirect(redirectTo);
-}
+// 	redirect(redirectTo);
+// }
