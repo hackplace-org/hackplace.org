8b8da8bc44313b0dd45f426b11b50df7a786ad7f
Anish Pallati <anishp@duck.com>
2023-09-24 13:11:27 -0400

    fix: Import issue in database schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,11 @@
 import { type InferInsertModel, sql } from "drizzle-orm";
 import { blob, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-import { type FormInput } from "@/app/onboarding/form";
+// import { type FormInput } from "@/app/onboarding/form";
+
+type FormInput = {
+	[key: string]: string;
+};
 
 export const users = sqliteTable("users", {
 	id: text("id").primaryKey(),
