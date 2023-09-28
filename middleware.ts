70cda3a511df62d890a69e69732b6d7548e0da6b
Anish Pallati <anishp@duck.com>
2023-09-27 21:39:21 -0400

    docs: Add comments for each branch of the middleware

--- middleware.ts
+++ middleware.ts
@@ -19,11 +19,15 @@ export default authMiddleware({
 
 		if (!auth.userId) {
 			if (pathname === "/onboarding") {
+				// If a user that has to complete onboarding signs out,
+				// then bring them back to the original page.
+				// If the original page is private, they will be redirected to sign in.
 				const url = new URL(searchParams.get("redirect") ?? "/", nextUrl);
 
 				return NextResponse.redirect(url);
 			}
 
+			// If the user has just signed out, don't do anything else
 			return;
 		}
 
@@ -33,6 +37,7 @@ export default authMiddleware({
 			pathname !== "/onboarding" &&
 			!user.privateMetadata.onboardingCompleted
 		) {
+			// If a user that has to complete onboarding tries to navigate to something else
 			const url = new URL("/onboarding", nextUrl);
 			url.searchParams.set("redirect", pathname);
 
@@ -41,6 +46,7 @@ export default authMiddleware({
 			pathname === "/onboarding" &&
 			user.privateMetadata.onboardingCompleted
 		) {
+			// If a user that has completed onboarding tries to navigate back to onboarding
 			return NextResponse.redirect(new URL("/", nextUrl));
 		}
 	},
