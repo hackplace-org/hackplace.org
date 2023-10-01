81d5ae28acf49f4ea34055f99a3062d32ed59dbe
Anish Pallati <anishp@duck.com>
2023-10-01 17:46:37 -0400

    feat: Create donate segment for EquiHacks page

--- app/equihacks/page.tsx
+++ app/equihacks/page.tsx
@@ -1,13 +1,21 @@
 import { currentUser } from "@clerk/nextjs";
+import { Rocket } from "lucide-react";
 import type { Metadata } from "next";
-import Link from "next/link";
+import NextLink from "next/link";
+import Marquee from "react-fast-marquee";
 
 import { UserTicket } from "@/app/equihacks/ticket";
 import { Content } from "@/components/content";
+import { Link } from "@/components/link";
 import { Navbar } from "@/components/navbar";
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
-import { Hover } from "@/components/utils";
-import Marquee from "react-fast-marquee";
+import { Grain, Heading, Hover } from "@/components/utils";
 
 export const metadata: Metadata = {
 	title: "EquiHacks",
@@ -40,18 +48,18 @@ export default async function EquiHacks() {
 				</hgroup>
 
 				<div className="mt-8 flex flex-row gap-x-4 justify-center">
-					<Link href="https://equihacks.devpost.com/" target="_blank">
+					<NextLink href="https://equihacks.devpost.com/" target="_blank">
 						<Button size="lg">Join now</Button>
-					</Link>
+					</NextLink>
 
-					<Link href="/donate">
+					<NextLink href="/donate">
 						<Button size="lg" variant="secondary">
 							Donate
 						</Button>
-					</Link>
+					</NextLink>
 				</div>
 
-				<div className="mt-16 flex flex-col gap-y-4 items-center">
+				<div className="mt-24 flex flex-col gap-y-4 items-center">
 					<h3 className="uppercase font-semibold">Thank you to our sponsors</h3>
 					<Marquee autoFill speed={25}>
 						<Button variant="ghost" className="mx-2">
@@ -74,8 +82,70 @@ export default async function EquiHacks() {
 						</Button>
 					</Marquee>
 				</div>
+			</Content>
+
+			<Content as="section" className="py-16" border="bottom">
+				<h1 className="p-4 mx-auto text-6xl font-bold text-center w-fit rounded-2xl bg-brand text-sky-900">
+					Your ticket
+				</h1>
 
 				{/* {user && <UserTicket username={user.username as string} />} */}
+			</Content>
+
+			<Content
+				as="section"
+				border="bottom"
+				className="flex py-16"
+				outerClassName="relative"
+			>
+				<Grain />
+				<div className="flex flex-col w-full text-primary">
+					<Heading>Help take EquiHacks further.</Heading>
+					<h2 className="my-4 text-2xl font-thin">
+						EquiHacks is made possible by the generous support of our sponsors.
+					</h2>
+
+					<Accordion type="single" className="w-full" collapsible>
+						<AccordionItem value="item-1">
+							<AccordionTrigger>
+								What kind of donations are you accepting?
+							</AccordionTrigger>
+							<AccordionContent>
+								Money, merch, perks/credits on your platform&mdash;you name it!
+								We&apos;re open to receiving anything that we can offer as a
+								prize.
+							</AccordionContent>
+						</AccordionItem>
+
+						<AccordionItem value="item-2">
+							<AccordionTrigger>
+								What will my donation be used for?
+							</AccordionTrigger>
+							<AccordionContent>
+								Your donations will be used to fund the prizes, venue, and other
+								expenses for EquiHacks. A full list of expenses can be found on
+								the <Link href="/donate" text="donations" /> page.
+							</AccordionContent>
+						</AccordionItem>
+
+						<AccordionItem value="item-3">
+							<AccordionTrigger>
+								I have further questions. Can I contact you?
+							</AccordionTrigger>
+							<AccordionContent>
+								Yes! Our contact information is available below.
+							</AccordionContent>
+						</AccordionItem>
+					</Accordion>
+
+					<NextLink href="/donate" className="mt-6">
+						<Button>Donate</Button>
+					</NextLink>
+				</div>
+
+				<div className="absolute top-0 right-0 -translate-x-6 -translate-y-20 text-muted -z-10">
+					<Rocket className="w-72 h-72" strokeWidth={0.75} />
+				</div>
 			</Content>
 		</>
 	);
