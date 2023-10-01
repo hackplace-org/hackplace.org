6618071986be1e1057e318c263eb3b127b7ea923
Anish Pallati <anishp@duck.com>
2023-10-01 16:52:29 -0400

    feat: Remove unused marquee animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,10 +63,6 @@ export default {
 				sm: "calc(var(--radius) - 4px)",
 			},
 			keyframes: {
-				marquee: {
-					from: { transform: "translateX(0%)" },
-					to: { transform: "translateX(calc(-100% - 1rem))" },
-				},
 				"accordion-down": {
 					from: { height: "0" },
 					to: { height: "var(--radix-accordion-content-height)" },
